83f89aff973ba1a687d63e9285af24e68b8c3b4d
howetuft <howetuft@gmail.com>
2025-09-01 06:10:44 +0200

    First stub (loadable by Blender, but without core gui)

---
+++ __init__.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2025 Authors (see AUTHORS file)
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+_needs_reload = "bpy" in locals()
+
+import bpy
+
+from . import ui
+
+if _needs_reload:
+    import importlib
+    ui = importlib.reload(ui)
+    print("BLC-Cockpit Reloaded")
+
+def register():
+    ui.register()
+
+def unregister():
+    ui.unregister()
