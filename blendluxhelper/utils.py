59239ae8c8dc288962a60403f16de320fbbfe29e
howetuft <howetuft@gmail.com>
2025-09-09 19:36:52 +0200

    Add  and  to utilities

--- blendluxhelper/utils.py
+++ blendluxhelper/utils.py
@@ -4,6 +4,8 @@
 
 import itertools
 
+import addon_utils
+
 ADDON_NAME = "BlendLuxHelper"
 
 def get_bl_idname():
@@ -12,3 +14,18 @@ def get_bl_idname():
     prefix = list(itertools.takewhile(lambda x: x != ADDON_NAME, components))
     prefix.append(ADDON_NAME)
     return '.'.join(prefix)
+
+def get_blc_module():
+    """Get BlendLuxCore module."""
+    modules = (m for m in addon_utils.modules() if m.bl_info["name"] == "BlendLuxCore")
+    for module in modules:
+        break
+    else:
+        module = None
+    return module
+
+def get_user_dir(name):
+    """Get a user writeable directory, create it if not existing."""
+    return pathlib.Path(
+        bpy.utils.extension_path_user("blendluxcore", path=name, create=True)
+    )
